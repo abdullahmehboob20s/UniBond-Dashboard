9227dbc24f08ed6da2c33ce5e7907cc889992d85
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-09 19:36:28 +0500

    added scrolbar hide functionality when sidebar open

--- layouts/SidebarLeft.js
+++ layouts/SidebarLeft.js
@@ -7,6 +7,7 @@ import OutsideClickDetector from "hooks/OutsideClickDetector";
 import SidebarFooter from "./SidebarFooter";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "reduxState/slices/themeSlice";
+import { useEffect } from "react";
 
 function SidebarLeft(props) {
   const [state, stateSetter] = props.state;
@@ -18,57 +19,74 @@ function SidebarLeft(props) {
     dispatch(toggleTheme(!isDark));
   };
 
+  useEffect(() => {
+    if (state) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "scroll";
+    }
+  }, [state]);
+
   return (
     <div
-      className={`${styles.sidebarLeft} ${state ? styles.open : ""}`}
-      ref={leftSidebarRef}
+      className={`${styles.wrapper} ${
+        state ? styles.wrapperShowing : null
+      } custom-scrollbar`}
     >
-      <header>
-        <IconButton icon="icons/moonIcon.svg" onClick={themeChanger} />
-        <button className={styles.closeBtn} onClick={() => stateSetter(false)}>
-          <IoClose />
-        </button>
-      </header>
+      <div
+        className={`${styles.sidebarLeft} ${state ? styles.open : ""}`}
+        ref={leftSidebarRef}
+      >
+        <header>
+          <IconButton icon="icons/moonIcon.svg" onClick={themeChanger} />
+          <button
+            className={styles.closeBtn}
+            onClick={() => stateSetter(false)}
+          >
+            <IoClose />
+          </button>
+        </header>
 
-      <main>
-        <Link href="/" passHref>
-          <a href="" className={`fs-16px black weight-6 ${styles.active}`}>
-            Dashboard
-          </a>
-        </Link>
-        <Link href="/" passHref>
-          <a href="" className="fs-16px black weight-6">
-            Home
-          </a>
-        </Link>
-        <Link href="/services" passHref>
-          <a href="" className="fs-16px black weight-6">
-            Services
-          </a>
-        </Link>
-        <Link href="/" passHref>
-          <a href="" className="fs-16px black weight-6">
-            Blog
-          </a>
-        </Link>
-        <Link href="/" passHref>
-          <a href="" className="fs-16px black weight-6">
-            Community
-          </a>
-        </Link>
-        <Link href="/location" passHref>
-          <a href="" className="fs-16px black weight-6">
-            Address
-          </a>
-        </Link>
-        <Link href="/" passHref>
-          <a href="" className="fs-16px black weight-6">
-            Payment
-          </a>
-        </Link>
-      </main>
+        <main>
+          <Link href="/" passHref>
+            <a href="" className={`fs-16px black weight-6 ${styles.active}`}>
+              Dashboard
+            </a>
+          </Link>
+          <Link href="/" passHref>
+            <a href="" className="fs-16px black weight-6">
+              Home
+            </a>
+          </Link>
+          <Link href="/services" passHref>
+            <a href="" className="fs-16px black weight-6">
+              Services
+            </a>
+          </Link>
+          <Link href="/" passHref>
+            <a href="" className="fs-16px black weight-6">
+              Blog
+            </a>
+          </Link>
+          <Link href="/" passHref>
+            <a href="" className="fs-16px black weight-6">
+              Community
+            </a>
+          </Link>
+          <Link href="/location" passHref>
+            <a href="" className="fs-16px black weight-6">
+              Address
+            </a>
+          </Link>
+          <Link href="/" passHref>
+            <a href="" className="fs-16px black weight-6">
+              Payment
+            </a>
+          </Link>
+        </main>
 
-      <SidebarFooter className={styles.footer} />
+        <SidebarFooter className={styles.footer} />
+      </div>
     </div>
   );
 }
