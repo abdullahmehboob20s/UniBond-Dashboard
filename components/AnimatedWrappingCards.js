6f2b821bb05e4122bed92dbc2f454cd108ab3fcc
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-09 19:11:43 +0500

    testing 2

--- components/AnimatedWrappingCards.js
+++ components/AnimatedWrappingCards.js
@@ -68,7 +68,7 @@ export default function AnimatedWrappingCards() {
 
   return (
     <div className={styles.AnimatedWrappingCards}>
-      <div className={styles.mainBox}>
+      {/* <div className={styles.mainBox}>
         <motion.div
           variants={checkState()}
           initial="initial"
@@ -100,7 +100,17 @@ export default function AnimatedWrappingCards() {
             isYellowCard={true}
           />
         </motion.div>
-      </div>
+      </div> */}
+
+      <CompanyProgressCard
+        title="Total Audit Ongoing"
+        subtitle="05"
+        inWeek="8"
+        iconWidth="36%"
+        icon="icons/total-audit-icon.png"
+        style={{ height: "100%" }}
+        isYellowCard={true}
+      />
     </div>
   );
 }
